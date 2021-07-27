72a89f1f3e73c06b62bcc2e9f39c697be3d655cf
Nick Nemtcev <nicknemtcev@outlook.com>
2021-07-27 12:19:24 +0700

    Add email to second section

---
+++ index.js
@@ -0,0 +1,10 @@
+import Typed from "typed";
+
+var options = {
+  strings: ["I am a software engineer"],
+  typeSpeed: 40,
+};
+
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+});
